fdff0bd59deee31e91b6b48508f49c5dce1edf41
Yourmei <405140692@qq.com>
2018-12-27 22:54:49 +0800

    mgb pom
    重新写pom.xml文件，解决无法找到主类的问题。
    在url后面加上参数useSSL=false

--- src/main/java/yourmei/test/MGBTest.java
+++ src/main/java/yourmei/test/MGBTest.java
@@ -15,15 +15,17 @@ import org.mybatis.generator.internal.DefaultShellCallback;
 
 public class MGBTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, XMLParserException, Exception {
+	
 	   List<String> warnings = new ArrayList<String>();
 	   boolean overwrite = true;
-	   File configFile = new File("mbg.xml");
+	   File configFile = new File("mgb.xml");
 	   ConfigurationParser cp = new ConfigurationParser(warnings);
 	   Configuration config = cp.parseConfiguration(configFile);
 	   DefaultShellCallback callback = new DefaultShellCallback(overwrite);
 	   MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
 	   myBatisGenerator.generate(null);
+	   System.out.println("success");
 	}
 
 }
