0153e20be7e4670aefaeec6b65f1d173d789de94
Yourmei <405140692@qq.com>
2019-01-06 20:52:24 +0800

    Add JUnit4  Test
    Add JUnit4  Test

---
+++ src/main/java/yourmei/test/MapperTest.java
@@ -0,0 +1,17 @@
+package yourmei.test;
+
+import org.junit.Test;
+
+public class MapperTest {
+
+	public static void main(String[] args) {
+		System.out.println("CRUD mapper Test");
+	}
+	
+	@Test
+	public void CRUDTest()
+	{
+		System.out.println("CRUD mapper Test");
+	}
+	
+}
