7de6f08ff8140900b19bc3b7e4e6b41107a30f11
Yourmei <405140692@qq.com>
2019-01-06 21:51:04 +0800

    mybatis test
    1.add spring-test(JUnit)
    2.test mybatis mapper

--- src/main/java/yourmei/test/MapperTest.java
+++ src/main/java/yourmei/test/MapperTest.java
@@ -1,17 +1,33 @@
 package yourmei.test;
 
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import caffrey.bean.Department;
+import caffrey.dao.DepartmentMapper;
+
 import org.junit.Test;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations= {"classpath:applicationContext.xml"})
 public class MapperTest {
-
-	public static void main(String[] args) {
-		System.out.println("CRUD mapper Test");
-	}
+	
+	@Autowired
+	DepartmentMapper departmentmapper;
 	
 	@Test
 	public void CRUDTest()
 	{
-		System.out.println("CRUD mapper Test");
+		System.out.println("CRUD mapper Test Start");
+		System.out.println(departmentmapper);
+		
+		Department dept1 = departmentmapper.selectByPrimaryKey(4);
+		System.out.println(dept1);
+		
+		Department dept2 = departmentmapper.selectByPrimaryKey(3);
+		System.out.println(dept2);
 	}
 	
 }
